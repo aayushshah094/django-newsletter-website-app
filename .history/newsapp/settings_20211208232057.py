3a34c233f46a5e584c63d379daa4ef562111abad
AayushKumar S Shah <aayushshah094@gmail.com>
2021-12-08 23:21:10 -0500

    setting

--- .history/newsapp/settings_20211208232057.py
+++ .history/newsapp/settings_20211208232057.py
@@ -135,7 +135,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/images/'
