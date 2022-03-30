efd0a83be16af7c3204633434f5d6bd831f37b59
AayushKumar S Shah <aayushshah094@gmail.com>
2022-03-30 13:13:33 -0400

    Update settings.py

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aayushshah490490@gmail.com'
-EMAIL_HOST_PASSWORD = 'Jayshah1@'
+EMAIL_HOST_USER = 'something@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
